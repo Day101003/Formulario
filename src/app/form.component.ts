cd991a078934a3138b13eeea3f163f6221f5f26f
Kendall Fernandez <kendall.fernandez@factork-software.com>
2026-01-21 15:48:35 -0600

    hotfix: Añadida las categorias con id correctas según las ids que expone la api de escuela JS

--- src/app/form.component.ts
+++ src/app/form.component.ts
@@ -27,7 +27,7 @@ export class FormComponent {
         description: formData.description || '',
         categoryId: Number(formData.categoryId),
         images: [
-          'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          'https://placehold.co/600x400',
         ],
       };
       // Copilot me explico como consumir por medio de fecth la API de escuela JS
